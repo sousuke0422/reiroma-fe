602dded2fd85d79043904d1c9ad68464a7068bf7
shpuld <shp@cock.li>
2019-06-17 21:20:23 +0300

    fix test to use now tolerance

--- test/unit/specs/services/date_utils/date_utils.spec.js
+++ test/unit/specs/services/date_utils/date_utils.spec.js
@@ -1,12 +1,12 @@
 import * as DateUtils from 'src/services/date_utils/date_utils.js'
 
-describe.only('DateUtils', () => {
+describe('DateUtils', () => {
   describe('relativeTime', () => {
-    it('returns now with low amount of seconds', () => {
+    it('returns now with low enough amount of seconds', () => {
       const futureTime = Date.now() + 20 * DateUtils.SECOND
       const pastTime = Date.now() - 20 * DateUtils.SECOND
-      expect(DateUtils.relativeTime(futureTime)).to.eql({ num: 0, key: 'time.now' })
-      expect(DateUtils.relativeTime(pastTime)).to.eql({ num: 0, key: 'time.now' })
+      expect(DateUtils.relativeTime(futureTime, 30)).to.eql({ num: 0, key: 'time.now' })
+      expect(DateUtils.relativeTime(pastTime, 30)).to.eql({ num: 0, key: 'time.now' })
     })
 
     it('rounds down for past', () => {
