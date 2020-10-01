636c4941c581d3f3a00ca657f616b1648ecac58d
Mark Felder <feld@FreeBSD.org>
2020-10-01 17:31:11 -0500

    Fix styling of ToS panel for consistency

--- src/components/terms_of_service_panel/terms_of_service_panel.vue
+++ src/components/terms_of_service_panel/terms_of_service_panel.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <div class="panel panel-default">
-      <div class="panel-body">
-        <!-- eslint-disable vue/no-v-html -->
-        <div
-          class="tos-content"
-          v-html="content"
-        />
-      <!-- eslint-enable vue/no-v-html -->
+    <div class="panel panel-default base01-background">
+      <div class="panel-heading timeline-heading base02-background">
+        <div class="panel-body">
+          <div class="title">
+            {{ $t('tos_panel.title') }}
+          </div>
+        </div>
       </div>
+      <!-- eslint-disable vue/no-v-html -->
+      <div
+        class="tos-content"
+        v-html="content"
+      />
+      <!-- eslint-enable vue/no-v-html -->
     </div>
   </div>
 </template>
